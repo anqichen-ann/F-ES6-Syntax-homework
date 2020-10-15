dce79dd10fe351c323a12dbb8e00884793988023
Anqi Chen <anqi.chen@AnqideMacBook-Pro.local>
2020-10-15 21:34:32 +0800

    [anqi] ES6-Syntax

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,18 @@
-const parseData
+const parseData = function(input){
+    let {data, column} = input;
+    let keys = [];
+    column.map( item => {
+        keys.push(item.name)
+    })
+    let output = [];
+    data.map( item => {
+        let tmp = {};
+        item.map( (value,index) => {
+            tmp[keys[index]] = value
+        } )
+        output.push(tmp)
+    })
+    return output;
+
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,16 @@
-const inject
+const inject = (items, sections) => {
+    let map = new Map();
+    let output = [];
+    sections.map( item => {
+        let {content,index} = item;
+        map.set(index,content)
+    })
+    items.map( (item,index) => {
+        if(map.get(index)){
+            output.push(map.get(index))
+        }
+        output.push(item)
+    })
+    return output;
+}
 export { inject };
